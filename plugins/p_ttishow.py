be67b343a999129e59c2332a25bcadf82e491faf
SahanS <87028927+sahansbandara@users.noreply.github.com>
2023-08-01 20:41:51 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('⚡ UᎮDΛTΞS ⚡', url='https://t.me/SinhalaCryptoNews')
+            InlineKeyboardButton('Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('Uᴘᴅᴀᴛᴇѕ', url='https://t.me/SECLK')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
