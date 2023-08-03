1cade8fa767a575c2a4e2171dc02b96a493cd782
Tashith Manuka <118094017+TashithManuka2006@users.noreply.github.com>
2023-08-03 09:35:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sahansbandara/NetflixlkBot
+  git clone https://github.com/TashithManuka2006/tsfilmsbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /NetflixlkBot
+  git clone $UPSTREAM_REPO /tsfilmsbot
 fi
-cd /NetflixlkBot
+cd /tsfilmsbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
